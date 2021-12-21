5be958d6a0e26bc8ad0f3869744056c0144e78c6
Anouchka <anouchka.maurer@protonmail.com>
2021-12-21 18:32:36 +0100

    rajout d'un peu de style CSS sur texte et boutton

--- meesith/src/components/PresProject.js
+++ meesith/src/components/PresProject.js
@@ -11,22 +11,22 @@ const PresProject = () => {
 				console.log(showLongVersion)
     }
     return (
-        <div>
+        <div className="txt">
             <p className="txtshort">
-								L’amour peut être si frustrant !
-
-                Votre fantasme est branché biotech alors que vous êtes certifié Bio ?
-                Il est monogame alors que vous êtes polyamoureuse ?
-                Sa sapiosexualité la fait passer à côté de vos plus belles qualités ?
-                Iel souhaite s’exiler sur Tatooine pour élever des banthas, alors que vous rêvez de vivre dans les plus hauts étages de Galactic City à Coruscant ?
-
+								L’amour peut être si frustrant !<br></br>
+								<br></br>
+                Votre fantasme est branché biotech alors que vous êtes certifié Bio ?<br></br>
+                Il est monogame alors que vous êtes polyamoureuse ?<br></br>
+                Sa sapiosexualité la fait passer à côté de vos plus belles qualités ?<br></br>
+                Iel souhaite s’exiler sur Tatooine pour élever des banthas, alors que vous rêvez de vivre dans les plus hauts étages de Galactic City à Coruscant ?<br></br>
+								<br></br>
                 Grâce à d’exceptionnelles avancées permises par les midi-chloriens, MeeSith a LA solution idéale à vous proposer pour un futur full love, siths & robots :
             </p>
             <p className={showLongVersion ? "txtlong" : "txthidden"}>
-								Cloner votre Sith, Jedi ou alien favori et le programmer afin qu’iel corresponde à vos désirs les plus fous !
-
-                En bonus, parce que nous vous aimons et souhaitons répandre l’Amour parfait à travers l’infini et l’au-delà, MeeSith vous propose un service unique :
-
+								<br></br>Cloner votre Sith, Jedi ou alien favori et le programmer afin qu’iel corresponde à vos désirs les plus fous !<br></br>
+								<br></br>
+                En bonus, parce que nous vous aimons et souhaitons répandre l’Amour parfait à travers l’infini et l’au-delà, MeeSith vous propose un service unique :<br></br>
+								<br></br>
                 Tester un ou plusieurs exemplaires de votre choix durant une période maximale de 10 jours, au sein d’un dôme reproduisant les caractéristiques de votre planète favorite.</p>
             <button onClick={display}>
 								{showLongVersion ? "Réduire" : "En savoir plus"} 
