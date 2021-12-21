afbf17b7c8ef8044dd570c634509b18fe96ba07f
Marine Grandcolas <marine-grandcolas_student2021@wilder.school>
2021-12-21 23:27:21 +0100

    updated logo reactions

--- meesith/src/components/Logo.js
+++ meesith/src/components/Logo.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import LogoMain from '../assets/logo.png';
 import LogoSlogan from '../assets/logo_slogan.png';
-import LogoBg from '../assets/logo_bg.png';
 import './Logo.css';
 
 const Logo = () => {
     return (
+        <div className="overflow">
         <div className="logo">
             <img className="logoMain" src={LogoMain} alt="logo"/>
                 <img className="logoSlogan" src={LogoSlogan} alt="logo slogan"/>
                 <div className="blackSquare"></div>
-            <img className="logoBg" src={LogoBg} alt="logo background"/>
+    
+        </div>
         </div>
     )
 }
