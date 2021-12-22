eb7d2c0417d80c06b350ee599e953d49050244c1
Anouchka <anouchka.maurer@protonmail.com>
2021-12-22 03:26:36 +0100

    ajout de style sur boutons

--- meesith/src/screen/cart.js
+++ meesith/src/screen/cart.js
@@ -15,25 +15,25 @@ function Cart({cart, updateCart, cartPlace, updateCartPlace}) {
     return (
         <div className='lmj-cart'>
             <Logo className="logo"/>
-            <h2>Panier</h2>
+            <h2>Votre futur</h2>
             <div className="clone">
-                Votre/Vos amour(s) de clone(s) :<br></br> {clonePrice} GaLacticCoins l'unité
-                <button onClick={() => updateCart(cart + 1)}>
-                    Ajouter
+                Votre/Vos amour(s) de clone(s) :<br></br> {clonePrice} GaLactiCoins l'unité
+                <button class="glow-on-hover" onClick={() => updateCart(cart + 1)}>
+                    AJOUTER
                 </button>
-                {cart > 0 ? <button onClick={() => updateCart(cart - 1)}>
-                    Retirer
-                    </button> : <div>Rendez-vous {<Link to="/Clones">ici</Link>} pour trouver le clone de votre vie !</div>
+                {cart > 0 ? <button class="glow-on-hover" onClick={() => updateCart(cart - 1)}>
+                    SUPPRIMER
+                    </button> : <div>Rendez-vous {<Link to="/Clones" className="ici">ici</Link>} pour trouver le clone de votre vie !</div>
                 }
             </div>
             <div className="place">
-                10 jours de test dans un lieu de rêve :<br></br> {placePrice} GaLacticCoins par séjour
-                <button onClick={() => updateCartPlace(cartPlace + 1)}>
-                    Ajouter
+                10 jours de test dans un lieu de rêve :<br></br> {placePrice} GaLactiCoins par séjour
+                <button class="glow-on-hover" onClick={() => updateCartPlace(cartPlace + 1)}>
+                    AJOUTER
                 </button>
-                {cartPlace > 0 ? <button onClick={() => updateCartPlace(cartPlace - 1)}>
-                    Retirer
-                </button> : <div>Choisissez {<Link to="/Places">ici</Link>} votre lieu rêvé pour du love +++</div>}
+                {cartPlace > 0 ? <button class="glow-on-hover" onClick={() => updateCartPlace(cartPlace - 1)}>
+                    SUPPRIMER
+                </button> : <div>Choisissez {<Link to="/Places" className="ici">ici</Link>} votre lieu rêvé pour du love +++</div>}
                 {console.log(cart, cartPlace)}
             </div>
             <h3>Total : {(clonePrice * cart) + (placePrice * cartPlace)} GLC</h3>
