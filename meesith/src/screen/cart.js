48d78d3577136f827c5a22675b1b976c1fe26849
Anouchka <anouchka.maurer@protonmail.com>
2021-12-22 12:42:35 +0100

    correction erreur

--- meesith/src/screen/cart.js
+++ meesith/src/screen/cart.js
@@ -2,7 +2,7 @@ import React from "react"
 import Logo from "../components/Logo"
 import {Link} from "react-router-dom"
 import { useState } from 'react'
-import "../components/Cart.css" 
+import "./Cart.css" 
 // import { useRouteMatch } from "react-router-dom"
 
 function Cart({cart, updateCart, cartPlace, updateCartPlace}) {
@@ -18,20 +18,20 @@ function Cart({cart, updateCart, cartPlace, updateCartPlace}) {
             <h2>Votre futur</h2>
             <div className="clone">
                 Votre/Vos amour(s) de clone(s) :<br></br> {clonePrice} GaLactiCoins l'unité
-                <button class="glow-on-hover" onClick={() => updateCart(cart + 1)}>
+                <button className="glow-on-hover" onClick={() => updateCart(cart + 1)}>
                     AJOUTER
                 </button>
-                {cart > 0 ? <button class="glow-on-hover" onClick={() => updateCart(cart - 1)}>
+                {cart > 0 ? <button className="glow-on-hover" onClick={() => updateCart(cart - 1)}>
                     SUPPRIMER
                     </button> : <div>Rendez-vous {<Link to="/Clones" className="ici">ici</Link>} pour trouver le clone de votre vie !</div>
                 }
             </div>
             <div className="place">
                 10 jours de test dans un lieu de rêve :<br></br> {placePrice} GaLactiCoins par séjour
-                <button class="glow-on-hover" onClick={() => updateCartPlace(cartPlace + 1)}>
+                <button className="glow-on-hover" onClick={() => updateCartPlace(cartPlace + 1)}>
                     AJOUTER
                 </button>
-                {cartPlace > 0 ? <button class="glow-on-hover" onClick={() => updateCartPlace(cartPlace - 1)}>
+                {cartPlace > 0 ? <button className="glow-on-hover" onClick={() => updateCartPlace(cartPlace - 1)}>
                     SUPPRIMER
                 </button> : <div>Choisissez {<Link to="/Places" className="ici">ici</Link>} votre lieu rêvé pour du love +++</div>}
                 {console.log(cart, cartPlace)}
